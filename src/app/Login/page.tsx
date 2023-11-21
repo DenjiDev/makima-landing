a5fe56969a508b690a19fea80a331788b93d4a61
AllanVargens <106094166+AllanVargens@users.noreply.github.com>
2023-11-21 16:09:05 -0300

    primeiro commit

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -2,41 +2,43 @@ import { Box, Button, Flex, FormControl, FormLabel, HStack, Heading, IconButton,
 import Link from "next/link";
 import { IoArrowBack } from "react-icons/io5";
 
-export default function PageLogin(){
-    return (
-        <HStack w='full' h='100vh'>
-            <Flex w='full' h='full' background='gray' align='center' justify='center' direction='column' gap={2} display={{base: 'none', md: 'flex'}}>
-                <p>Imagem</p>
-                <Button gap={2}>
-                    Back to Home page
-                    <IoArrowBack />
-                </Button>
-            </Flex>
-            <Flex w='full' h='full' alignItems='center' justifyContent='center'>
-                <Stack w='full' maxW='md' spacing={4} p={6}>
-                    <Heading fontSize='2xl' color='black'>Sign in to your account</Heading>
-                    <FormControl id="user">
-                        <FormLabel>Username</FormLabel>
-                        <Input placeholder="Username"/>
-                    </FormControl>
-                    <FormControl id="password">
-                        <FormLabel>Password</FormLabel>
-                        <Input type="password" placeholder="**********"/>
-                    </FormControl>
-                    <Stack
-                    spacing={4}
-                    direction='row'
-                    align='start'
-                    justify='space-between'
-                    >
-                        <Link href='#'>Forgot Password?</Link>
-                    </Stack> 
+export default function PageLogin() {
+  return (
+    <HStack w='full' h='100vh'>
+      <Flex w='full' h='full' background='gray' align='center' justify='center' direction='column' gap={2} display={{ base: 'none', md: 'flex' }}>
+        <p>Imagem</p>
+        <Button gap={2} w='max-content'>
+          <Link href='/'>
+            Home page
+            <IoArrowBack />
+          </Link>
+        </Button>
+      </Flex>
+      <Flex w='full' h='full' alignItems='center' justifyContent='center'>
+        <Stack w='full' maxW='md' spacing={4} p={6}>
+          <Heading fontSize='2xl' color='black'>Sign in to your account</Heading>
+          <FormControl id="user">
+            <FormLabel>Username</FormLabel>
+            <Input placeholder="Username" />
+          </FormControl>
+          <FormControl id="password">
+            <FormLabel>Password</FormLabel>
+            <Input type="password" placeholder="**********" />
+          </FormControl>
+          <Stack
+            spacing={4}
+            direction='row'
+            align='start'
+            justify='space-between'
+          >
+            <Link href='#'>Forgot Password?</Link>
+          </Stack>
 
-                    <Button>
-                        Sign in
-                    </Button>       
-                </Stack>
-            </Flex>
-        </HStack>
-    )
+          <Button>
+            Sign in
+          </Button>
+        </Stack>
+      </Flex>
+    </HStack>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import Head from "next/head";
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>Home Page | Makima</title> 
-    </Head> 
-    <Box as="main">
-      <h1>this is the Home Page</h1>
-    </Box>
+      <Head>
+        <title>Home Page | Makima</title>
+      </Head>
+      <Box as="main">
+        <h1>this is the Home Page</h1>
+      </Box>
     </>
   )
 }
